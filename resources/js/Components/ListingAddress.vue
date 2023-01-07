5155803c46da492f7f68d870b806975d7f06ab06
Saulo Silva <saulo.silva@smartload.com.br>
2023-01-07 16:19:42 -0300

    tailwind css

--- resources/js/Components/ListingAddress.vue
+++ resources/js/Components/ListingAddress.vue
@@ -1,12 +1,11 @@
 <template>
   <p>
-    {{ listing.street }} {{ listing.street_number }}, {{ listing.city }}, for
-    ${{ listing.price }}
+    {{ listing.street }} {{ listing.street_number }}, {{ listing.city }}
   </p>
 </template>
 
 <script setup>
 defineProps({
   listing: Object,
-});
+})
 </script>
